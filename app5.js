480e9d0abe32d474c48459045ea0a50da9b88a91
Aadarsh <aadarsh.s2019@vitbhopal.ac.in>
2020-06-27 23:15:29 +0530

    first commit

---
+++ app5.js
@@ -0,0 +1,8 @@
+var form = document.querySelector('#libForm');
+form.addEventListener('submit',function (e) {
+  if (!form.checkValidity()){
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  form.classList.add('was-validated');
+})
